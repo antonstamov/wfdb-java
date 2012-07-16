92ae644d65677a55746c2472c05ba060576ed2fc
ikarosilva00@gmail.com <ikarosilva00@gmail.com@d1ef6642-ba91-2670-da20-c759dc64a47e>
2012-07-16 13:47:14 +0000

    skeleton, rdsamp, wfdbdesc

---
+++ trunk/src/org/physionet/wfdb/wfdbdesc.java
@@ -0,0 +1,52 @@
+/* ===========================================================
+ * WFDB Java : Interface to WFDB Applications.
+ *              
+ * ===========================================================
+ *
+ * (C) Copyright 2012, by Ikaro Silva
+ *
+ * Project Info:
+ *    Code: http://code.google.com/p/wfdb-java/
+ *    WFDB: http://www.physionet.org/physiotools/wfdb.shtml
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * Original Author:  Ikaro Silva
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * Check: http://code.google.com/p/wfdb-java/list
+ */ 
+package org.physionet.wfdb;
+
+/**
+ * @author ikaro
+ *
+ */
+public class wfdbdesc extends wfdbexec {
+
+	private static final String TAG="wfdbdesc";
+	
+    public static void main(String[] args) {
+    	//Initialize base class
+    	wfdbdesc my_wfdbdesc=new wfdbdesc();	
+    	//Run executable
+    	System.out.println(my_wfdbdesc.output(TAG,args[0]));
+    }
+
+
+}
