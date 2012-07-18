9fac598d0874ab6e14f8d4ffc0cedff4e25166a6
ikarosilva00 <ikarosilva00@gmail.com>
2012-07-18 20:47:48 +0000

    Pushed the exec and gen_exec from all classes to Wfdbexec

--- src/org/physionet/wfdb/examples/RdsampEx1.java
+++ src/org/physionet/wfdb/examples/RdsampEx1.java
@@ -4,12 +4,13 @@ import org.physionet.wfdb.Rdsamp;
 public class RdsampEx1 {
 
 	public static void main(String[] args) {
-		// Simple test of for the class Reads first 10 seconds of ECG
+		// Simple test of for the class
 		Rdsamp rdsampexec = new Rdsamp();
 		rdsampexec.setArgumentValue(Rdsamp.Arguments.stopTime, "10s");
 		rdsampexec.setArgumentValue(Rdsamp.PrintTimeFormatLabel.p);
 		rdsampexec.setArgumentValue(Rdsamp.Arguments.recordName, "mitdb/100");
 		System.out.println(rdsampexec.exec());
+		
 	}
 	
 }
