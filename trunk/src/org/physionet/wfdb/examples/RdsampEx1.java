bafd65f949ee3737d80d65217f33dbc70e01e8b2
ikarosilva00@gmail.com <ikarosilva00@gmail.com@d1ef6642-ba91-2670-da20-c759dc64a47e>
2012-07-20 15:23:22 +0000

    deleting unused resources

--- trunk/src/org/physionet/wfdb/examples/RdsampEx1.java
+++ trunk/src/org/physionet/wfdb/examples/RdsampEx1.java
@@ -1,16 +1,17 @@
 package org.physionet.wfdb.examples;
 import org.physionet.wfdb.Rdsamp;
 
+
 public class RdsampEx1 {
 
 	public static void main(String[] args) {
 		// Simple test of for the class
+		
 		Rdsamp rdsampexec = new Rdsamp();
 		rdsampexec.setArgumentValue(Rdsamp.Arguments.stopTime, "10s");
 		rdsampexec.setArgumentValue(Rdsamp.PrintTimeFormatLabel.p);
 		rdsampexec.setArgumentValue(Rdsamp.Arguments.recordName, "mitdb/100");
 		System.out.println(rdsampexec.exec());
-		
 	}
 	
 }
