bb5a2d6a47ec4c1c7b21d7ea49641f3312f94ce6
ikarosilva00 <ikarosilva00@gmail.com>
2012-07-24 18:08:46 +0000

    added EDR binaries and source

--- nativelibs/build_wfdbjava.sh
+++ nativelibs/build_wfdbjava.sh
@@ -13,7 +13,7 @@ fi
 # FIXME: the version number changes!
 WFDB_VER="wfdb-10.5.13"
 
-
+SRC_LOCATION="/home/ikaro/workspace/wfdb-java/nativelibs/tarballs/"
 cd $1
 mkdir tmp
 cd tmp
@@ -23,7 +23,7 @@ cd tmp
 #
 #WGET may not work, depending on firewall settings
 #wget ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.10.tar.bz2
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/libgpg-error-1.10.tar.bz2 $1/tmp/
+cp $SRC_LOCATIONlibgpg-error-1.10.tar.bz2 $1/tmp/
 tar xvfj libgpg-error-1.10.tar.bz2
 cd libgpg-error-1.10
 ./configure --prefix=$1
@@ -36,7 +36,7 @@ cd ..
 # build libidn
 #
 #wget ftp://aeneas.mit.edu/pub/gnu/libidn/libidn-1.12.tar.gz
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/libidn-1.12.tar.gz $1/tmp/
+cp $SRC_LOCATIONlibidn-1.12.tar.gz $1/tmp/
 tar xvfz libidn-1.12.tar.gz
 cd libidn-1.12
 ./configure --prefix=$1
@@ -49,7 +49,7 @@ cd ..
 # build libgcrypt
 #
 #wget ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.4.4.tar.bz2
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/libgcrypt-1.4.4.tar.bz2 $1/tmp/
+cp $SRC_LOCATIONlibgcrypt-1.4.4.tar.bz2 $1/tmp/
 tar xvfj libgcrypt-1.4.4.tar.bz2
 cd libgcrypt-1.4.4
 ./configure --prefix=$1 --with-gpg-error-prefix=$1
@@ -62,7 +62,7 @@ cd ..
 # build gnutls
 #
 #wget http://ftp.gnu.org/pub/gnu/gnutls/gnutls-2.6.4.tar.bz2
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/gnutls-2.6.4.tar.bz2 $1/tmp/
+cp $SRC_LOCATIONgnutls-2.6.4.tar.bz2 $1/tmp/
 tar xvfj gnutls-2.6.4.tar.bz2
 cd gnutls-2.6.4
 ./configure --prefix=$1 --with-libgcrypt-prefix=$1 --without-zlib
@@ -75,7 +75,7 @@ cd ..
 # build curl
 #
 #wget http://curl.haxx.se/download/curl-7.19.3.tar.bz2
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/curl-7.19.3.tar.bz2 $1/tmp/
+cp $SRC_LOCATIONcurl-7.19.3.tar.bz2 $1/tmp/
 tar xvfj curl-7.19.3.tar.bz2
 cd curl-7.19.3
 ./configure --prefix=$1 --with-gnutls=$1 --with-libidn=$1 \
@@ -88,10 +88,18 @@ cd ..
 #
 # build wfdb
 #wget http://www.physionet.org/physiotools/wfdb.tar.gz
-cp /home/ikaro/workspace/wfdb-java/nativelibs/tarballs/wfdb.tar.gz $1/tmp/
+cp $SRC_LOCATIONwfdb.tar.gz $1/tmp/
 tar xvfz wfdb.tar.gz
 cd $WFDB_VER
 PATH=$1/bin:$PATH ./configure --prefix=$1 --with-libcurl
 PATH=$1/bin:$PATH make
 PATH=$1/bin:$PATH make install
 cd ..
+
+
+
+#
+# build edr
+#
+cc -o $1/bin/edr $SRC_LOCATIONedr.c -lm -lwfdb
+
